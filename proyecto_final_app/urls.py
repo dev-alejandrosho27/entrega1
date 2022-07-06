8e47c93674e894ad95c0ca56cabf304ca983a0f8
Alejandro S. Aguilar <alejo.san2707@gmail.com>
2022-07-06 10:04:51 -0300

    se creo la busqueda

--- proyecto_final_app/urls.py
+++ proyecto_final_app/urls.py
@@ -5,6 +5,7 @@ from django.urls import path,URLPattern
 from proyecto_final_app import views
 
 urlpatterns = [
-    path("/", views.index),
+    path("inicio/", views.index),
     path("busqueda/", views.busqueda_pokemon),
+    path("buscar/", views.buscar_pokemon),
 ]
--- proyecto_final_app/views.py
+++ proyecto_final_app/views.py
@@ -2,6 +2,8 @@ from django.template import loader
 from django.http import HttpResponse
 from django.shortcuts import render,HttpResponse
 
+from proyecto_final_app.models import pokemon
+
 # Create your views here.
 def index(request):
     return render(request, "proyecto_final_app/index.html")
@@ -10,4 +12,21 @@ def index(request):
 
 
 def busqueda_pokemon(request):
+    
     return render(request, "proyecto_final_app/busqueda.html")
+
+
+
+def buscar_pokemon(request):
+    
+    if request.GET["pokemon"]:
+        
+        mensaje="articulo buscado: %r" %request.GET["pokemon"]
+        pokemon_buscado=request.GET["pokemon"]
+        pokemon_Base_Datos=pokemon.objects.filter(nombre__icontains=pokemon_buscado)
+        return render(request, "proyecto_final_app/resultado_busqueda.html", {"pokemon buscado":pokemon_buscado,"pokemon en la base de datos":pokemon_Base_Datos})
+    else:
+        
+        mensaje="por favor introduce un pokemon a buscar"
+        
+    return HttpResponse(mensaje)
